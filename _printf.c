f7b3d55a081d0191baa1e791ce946e7c741b82a3
fady-helal <fadyhelal78>
2023-11-13 12:54:05 +0200

    update

--- _printf.c
+++ _printf.c
@@ -8,37 +8,41 @@
 
 int _printf(const char *format, ...)
 {
-    operation ops[] = {
-        {"%c", print_char},
-        {"%s", print_string},
-        {"%%", print_37},
-    };
-    va_list args;
-    int i, j;
-    int length = 0;
-    int found;
+operation ops[] = {
+{"%c", print_char},
+{"%s", print_string},
+{"%%", print_37},
+};
+va_list args;
+int i, j;
+int length = 0;
+int found;
 
-    va_start(args, format);
+va_start(args, format);
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 
-    for (i = 0; format[i] != '\0'; i++) {
-        found = 0;
-        for (j = 0; j < 3; j++) {
-            if (ops[j].c[0] == format[i] && ops[j].c[1] == format[i + 1]) {
-                length += ops[j].f(args);
-                i++;
-                found = 1;
-                break;
-            }
-        }
-        if (!found) {
-            _putchar(format[i]);
-            length++;
-        }
-    }
+for (i = 0; format[i] != '\0'; i++)
+{
+	found = 0;
+for (j = 0; j < 3; j++)
+{
+	if (ops[j].c[0] == format[i] && ops[j].c[1] == format[i + 1])
+	{
+		length += ops[j].f(args);
+		i++;
+		found = 1;
+		break;
+	}
+}
+if (!found)
+{
+	_putchar(format[i]);
+	length++;
+}
+}
 
-    va_end(args);
-    return (length);
+va_end(args);
+return (length);
 }
