c4d2afcfac1b17e61085688631218a75cfd3eb57
Anhar Diab <rivadiab30663@gmail.com>
2023-11-14 10:52:31 -0800

    new items

--- print_int.c
+++ print_int.c
@@ -3,7 +3,7 @@ int print_int(va_list ap, params_t *params)
 {
         long l;
 
-        if (params->L_modifier)
+        if (params->l_modifier)
                 l = va_arg(ap, long);
         else if (params->h_modifier)
                 l = (short int)va_arg(ap, int);
