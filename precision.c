c2ef8540526511cc34456f3aeb4585beb0829e84
Anhar Diab <rivadiab30663@gmail.com>
2023-11-14 10:33:40 -0800

    new items

---
+++ precision.c
@@ -0,0 +1,32 @@
+#include "main.h"
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t specifiers[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ print_from_to.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int print_from_to(char *start, char *stop, char *except)
+{
+	int sum = 0;
+
+	while (start <= stop)
+	{
+		if (start != except)
+			sum += _putchar(*start);
+		start++;
+	}
+	return (sum);
+}
